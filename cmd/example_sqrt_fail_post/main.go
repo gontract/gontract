c0d3d1c23c492f7ccb3bea9a325a5c82591dbef4
Michael Adam <obnox@samba.org>
2025-11-27 15:20:06 +0100

    add another example
    example of failingf postcondition in square root calculation.
    
    Signed-off-by: Michael Adam <obnox@samba.org>

---
+++ cmd/example_sqrt_fail_post/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/obnoxxx/gontract"
+)
+
+func CalculateSqrt(n float64) float64 {
+
+	gontract.Condition(n >= 0, gontract.KindPre, "square root undefined for negative numbers")
+
+	r := n * n
+
+	gontract.Condition(r*r == n, gontract.KindPost, "input must equal square of result")
+
+	return r
+}
+
+func main() {
+
+	var num float64 = 1
+
+	root := CalculateSqrt(num)
+
+	fmt.Printf("The square root of %f is %f\n", num, root)
+
+	num = 4
+
+	root = CalculateSqrt(num)
+
+	fmt.Printf("The square root of %f is %f\n", num, root)
+
+}
