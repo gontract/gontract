1c9d46bf123b5e0afb5c7ab3f85dd19991e227da
Michael Adam <obnox@samba.org>
2025-12-02 10:09:59 +0100

    tests: improve sqrt test
    This prepares the test for handling the panic case.
    
    Signed-off-by: Michael Adam <obnox@samba.org>

--- cmd/example_sqrt_success/main_test.go
+++ cmd/example_sqrt_success/main_test.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func CheckCalculateSqrt(num float64) (ret float64) {
+
+	ret = 0
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("CalculateSqrt paniced: '%s'", r.(string))
+			ret = -1
+		}
+	}()
+
+	ret = CalculateSqrt(num)
+	return
+}
+
 func TestCalculateSqrt(t *testing.T) {
+
 	var num float64 = 1
-	root := CalculateSqrt(num)
+	root := CheckCalculateSqrt(num)
 	assert.Equal(t, num, root*root)
 
-	num=4
-	root = CalculateSqrt(num)
+	num = 4
+	root = CheckCalculateSqrt(num)
 	assert.Equal(t, num, root*root)
 }
