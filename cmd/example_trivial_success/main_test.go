3b04ae21dc06b5154af430f3e3615d3d47fa799d
Michael Adam <obnox@samba.org>
2025-12-19 12:45:43 +0100

    examples: remove unused function
    This removes an unused function from test code
    
    Signed-off-by: Michael Adam <obnox@samba.org>

--- cmd/example_trivial_success/main_test.go
+++ cmd/example_trivial_success/main_test.go
@@ -3,20 +3,8 @@ package main
 import (
 	"testing"
 
-	"github.com/gontract/gontract"
 	"github.com/stretchr/testify/assert"
 )
-
-func catch_violation(num *float64) {
-
-	var str = "foo"
-
-	gontract.CatchViolation(&str)
-	if str != "foo" {
-		*num = -1.0
-	}
-
-}
 
 func Test_main(t *testing.T) {
 	main()
